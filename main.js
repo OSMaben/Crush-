2cdbd7566e4dea415fab947b294fa20e752f4341
oussama benmazzi <oussamabenmazzi223@gmail.com>
2023-01-31 21:50:52 +0100

    send this to your crush with no content

---
+++ main.js
@@ -0,0 +1,27 @@
+// no btn
+const buttonElement = document.querySelector(".no");
+let position = "";
+buttonElement.addEventListener("mouseover", function () {
+    if (position === 0) {
+        position = 400;
+    } else {
+        position = 0;
+    }
+    buttonElement.style.transform = `translate(${position}px , 100px)`;
+});
+
+// POp up
+let btn = document.querySelector('.yes');
+let popup = document.querySelector('.popup');
+btn.addEventListener('click', show);
+
+function show() {
+    popup.classList.add('show');
+    console.log('clicked');
+
+}
+window.onclick = function (event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
